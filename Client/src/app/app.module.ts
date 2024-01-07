60bcd6ca6511b878045b8b9a45ecabc49fd9c9a5
DESKTOP-FC6EDPL\User <truongthetan1601@gmail.com>
2024-01-07 17:05:19 +0700

    add bootstrap 5

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { AuthService, AA } from './services/auth.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, LoginComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        ReactiveFormsModule,
+    ],
+    providers: [AuthService],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
